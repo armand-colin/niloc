68591b9e6844b6198ae9b85aacc102f32046840e
armand-colin <56818352+armand-colin@users.noreply.github.com>
2023-04-16 12:05:18 +0200

    removed node_modules

--- chess/server/node_modules/.bin/nanoid.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\nanoid\bin\nanoid.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\nanoid\bin\nanoid.js" %*
-)
