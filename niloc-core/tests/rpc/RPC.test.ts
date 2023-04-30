2f8def4adfac5c2248f9a281b520d0286abec7af
armand-colin <56818352+armand-colin@users.noreply.github.com>
2023-04-30 15:58:27 +0200

    added tests

--- niloc-core/tests/rpc/RPC.test.ts
+++ niloc-core/tests/rpc/RPC.test.ts
@@ -2,6 +2,9 @@ import { describe, expect, it } from "vitest";
 import { MockChannels } from "../channel/MockChannels";
 import { RPCHandler } from "../../lib/rpc/RPCHandler"
 import { RPC } from "../../lib/rpc/RPC";
+import { RPCPlugin } from "../../lib/rpc/RPCPlugin";
+import { MockModel } from "../sync/MockModel";
+import { SyncObject, Template } from "../../lib/main";
 
 describe('RPC', () => {
 
@@ -25,6 +28,38 @@ describe('RPC', () => {
         expect(await rpcA.call(0)).to.equal('a0')
         expect(await rpcA.call(10)).to.equal('a10')
         expect(await rpcB.call(0)).to.equal('a0')
+    })
+
+    class Avatar extends SyncObject {
+
+        static template = Template.create('avatar', Avatar)
+
+        isHost = false
+
+        ping = new RPC(() => {
+            if (this.isHost)
+                return 'pong'
+            throw "Called RPC from peer b"
+        }, 'a')
+
+    }
+
+    it('Should work with a model', async () => {
+        const [channelA, channelB] = MockModel.channels('a', 'b')
+        const [modelA, modelB] = MockModel.models([Avatar.template])
+
+        modelA.plugin(new RPCPlugin('a', channelA))
+        modelB.plugin(new RPCPlugin('b', channelB))
+
+        const avatarA = modelA.instantiate(Avatar.template, 'avatarA')
+        avatarA.isHost = true
+        modelA.tick()
+
+        {
+            const avatarA = modelB.get<Avatar>("avatarA")!
+            expect(avatarA).not.to.be.null;
+            expect(await avatarA.ping.call()).to.equal("pong")
+        }
     })
 
 })
