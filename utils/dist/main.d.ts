36011809e3e79cf5f6c58ec5e716c3584db8b0f7
armand-colin <56818352+armand-colin@users.noreply.github.com>
2024-11-07 17:39:26 +0100

    deleted dist

--- utils/dist/main.d.ts
+++
@@ -1,6 +0,0 @@
-export { Emitter } from "./Emitter";
-export type { IEmitter } from "./Emitter";
-export { Provider } from "./Provider";
-export { Iter } from "./Iter";
-export { Observable } from "./observable/Observable";
-export { Transformer } from "./observable/Transformer";
